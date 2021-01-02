49f655826918bac8828e0decdc50fd0e1ef086ee
Bartosz Biegajło <biegbart.z@gmail.com>
2021-01-02 11:28:39 +0100

    add polish prompts and user panel view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card mycard">
+                <div class="card-header">{{ __('Panel użytkownika') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,11 +13,29 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <div class="d-flex flex-column">
+                        <div class="card-body-userdata d-flex flex-wrap">
+                            <img class="p-2" src="images/samples/userIcon.jpg" width="150" height="150" alt="sample user icon" style="flex: ">
 
-                    {{ __('You are logged in!') }}
+                            <div class="p-2">
+                                <h3>Dane użytkownika</h3>
+                                <table class="table">
+                                    <tr><td><b>Nazwa</b></td><td>{{ Auth::user()->name }}</td></tr>
+                                    <tr><td><b>Email</b></td><td>{{ Auth::user()->email }}</td></tr>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="card-body-interface d-flex flex-wrap">
+                            <a class="btn btn-link" href="#">
+                                Dodaj książkę
+                            </a>
+                            <a class="btn btn-link" href="#">
+                                Moja kolekcja
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     </div>
 </div>
 @endsection
